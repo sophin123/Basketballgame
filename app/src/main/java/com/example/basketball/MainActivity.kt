a8a3d1dc8203213fdd3d3ceb3b407e115cb8e2cb
sophin <sophinstha123@gmail.com>
2019-10-23 08:31:49 +1100

    Intent added

--- app/src/main/java/com/example/basketball/MainActivity.kt
+++ app/src/main/java/com/example/basketball/MainActivity.kt
@@ -1,12 +1,19 @@
 package com.example.basketball
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        getstartedBtn.setOnClickListener(){
+            val secondlayout = Intent(this, Main2Activity::class.java)
+            startActivity(secondlayout)
+        }
     }
 }
