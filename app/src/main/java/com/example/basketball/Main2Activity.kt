88c0fe5d89544331af3637e5fdfcb1d9cb421298
sophin <sophinstha123@gmail.com>
2019-10-25 07:40:23 +1100

    added skill layout

--- app/src/main/java/com/example/basketball/Main2Activity.kt
+++ app/src/main/java/com/example/basketball/Main2Activity.kt
@@ -1,12 +1,20 @@
 package com.example.basketball
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
-class Main2Activity : AppCompatActivity() {
+class Main2Activity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+
+    fun leaguetoskill(view: View){
+        val skillintent = Intent(this, SkillActivity::class.java)
+        startActivity(skillintent)
+
     }
 }
